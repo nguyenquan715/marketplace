ec0d6e2c8076e51493112ef56dc6d877a4962483
QuanNA <quanna@relipasoft.com>
2022-05-31 13:46:27 +0700

    wallet interaction

--- dapp/public/scripts/main.js
+++ dapp/public/scripts/main.js
@@ -1,4 +1,39 @@
+let connectedAccount;
 const connectWalletBtn = document.getElementById("connect-wallet");
-connectWalletBtn.onclick = () => {
 
+getWalletAccounts().then((accounts) => {
+  connectedAccount = accounts[0];
+  if (connectedAccount) {
+    displayAccountAddress(connectedAccount);
+  }
+}).catch((err) => {
+  console.error(err);
+});
+
+connectWalletBtn.onclick = async () => {
+  try {
+    console.log('Connect wallet');
+    connectedAccount = (await connectWallet())[0];
+    displayAccountAddress(connectedAccount);
+    await switchEthereumChain('0x13881', 'Mumbai', 'https://rpc-mumbai.matic.today');
+    window.ethereum.on('accountsChanged', async (accounts) => {
+      console.log(accounts);
+    });
+    window.ethereum.on('chainChanged', async (chainId) => {
+      if (await checkEthereumChain('0x13881')) return;
+      if (confirm("You must switch to Mumbai network!")) {
+        await switchEthereumChain('0x13881', 'Mumbai', 'https://rpc-mumbai.matic.today');
+      } else {
+        alert("You are in wrong network!");
+      }
+      window.location.reload();
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function displayAccountAddress(account) {
+  const accountAddress = account.slice(0, 5) + '...' + account.slice(account.length - 4, account.length);
+  connectWalletBtn.innerHTML = accountAddress;
 }
--- dapp/public/scripts/wallet-interaction.js
+++ dapp/public/scripts/wallet-interaction.js
@@ -10,7 +10,69 @@ const getWalletAccounts = () => {
   });
 }
 
-const checkConnectedWallet = () => {
-  
+const connectWallet = async () => {
+  try {
+    let accounts = await getWalletAccounts();
+    if (accounts.length > 0) {
+      return accounts;
+    }
+    accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts'
+    });
+    return accounts;
+  } catch (err) {
+    if (err.code === 4001) {
+      alert("Please connect to MetaMask!");
+    } else {
+      throw err;
+    }
+  }
+}
+
+const requestAccount = async () => {
+  return window.ethereum.request({
+    method: "wallet_requestPermissions",
+    params: [{ eth_accounts: {} }]
+  });
+}
+
+const addEthereumChain = async (chainId, chainName, chainRpc) => {
+  const chainInfo = {
+    chainId: chainId,
+    chainName: chainName,
+    rpcUrls: [chainRpc]
+  };
+  return window.ethereum.request({
+    method: 'wallet_addEthereumChain',
+    params: [
+      chainInfo
+    ]
+  });
+}
+
+const checkEthereumChain = async (chainId) => {
+  const currentChain = await window.ethereum.request({
+    method: 'eth_chainId'
+  });
+  if (currentChain === chainId) return true;
+  return false;
+}
+
+const switchEthereumChain = async (chainId, chainName, chainRpc) => {
+  try {
+    if (await checkEthereumChain(chainId)) return;
+    await window.ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId }]
+    });
+  } catch (err) {
+    if (err.code === 4902) {
+      await addEthereumChain(chainId, chainName, chainRpc).catch((err) => {
+        throw err;
+      });
+    } else {
+      throw err;
+    }
+  }
 }
 
